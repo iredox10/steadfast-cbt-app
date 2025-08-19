d35e553f974f3d1314c9490f27c2bcff9a81b046
iredox <idreesadam200@gmail.com>
2025-08-19 11:10:07 +0100

    change invigilator sidebar logo

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import logo from '../../public/assets/logo.webp';
+import logo from '../../public/assets/buk.png';
 import { FaSignOutAlt, FaQuestionCircle } from 'react-icons/fa';
 
 const Sidebar = ({ children }) => {
@@ -7,14 +7,14 @@ const Sidebar = ({ children }) => {
         <div className="w-64 bg-white flex-shrink-0 border-r border-gray-200 flex flex-col h-screen">
             <div className="flex flex-col items-center p-6 border-b border-gray-200">
                 <img src={logo} className="w-16 h-16 object-contain" alt="HUK POLY Logo" />
-                <h1 className="text-xl font-bold text-gray-900 mt-2">HUK POLY</h1>
+                <h1 className="text-xl font-bold text-gray-900 mt-2">BUK KANO</h1>
             </div>
-            
+
             <div className="flex flex-col justify-between flex-1">
                 <div className="flex flex-col gap-2 px-4 py-6">
                     {children}
                 </div>
-                
+
                 <div className="p-6 border-t border-gray-200">
                     <Link to="" className="flex items-center gap-3 px-4 py-3 text-gray-600 hover:bg-gray-100 rounded-lg transition-colors duration-200">
                         <FaQuestionCircle />
