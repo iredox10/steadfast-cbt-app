193a62e1e6406de51af96c673fd31a06c9007b0f
iredox <idreesadam200@gmail.com>
2025-08-21 12:15:01 +0100

    fix: not showing course(s) in instructor page

--- app/Http/Controllers/Instructor.php
+++ app/Http/Controllers/Instructor.php
@@ -206,11 +206,6 @@ class Instructor extends Controller
         }
     }
 
-        } catch (Exception $e) {
-            return response()->json($e->getMessage(), 400);
-        }
-    }
-
     public function submitExam(Request $request, $exam_id)
     {
         try {
