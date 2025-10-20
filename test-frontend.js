60631c05e9e57488b932925074b6ec0b45006d19
iredox <idreesadam200@gmail.com>
2025-10-20 12:32:10 +0100

    remove unwanted files

--- test-frontend.js
+++
@@ -1,5 +0,0 @@
-// Simple test to check if the student route is working
-fetch('http://localhost:8000/api/get-student-exam')
-  .then(response => response.json())
-  .then(data => console.log('API Response:', data))
-  .catch(error => console.error('API Error:', error));
